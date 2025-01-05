6ff3981b64fb1f0a9f7d893678469d3a06abffbf
Edgar Žigis <edgar.zigis@gmail.com>
2025-01-05 20:58:32 +0200

    Add project sources

---
+++ scripts/bioclimatic_variable.R
@@ -0,0 +1,25 @@
+# Bioclimatic variable names according to the historical climate data
+# Resolution - 5 minutes
+# https://www.worldclim.org/data/worldclim21.html
+
+BioClimaticVariable <- list(
+  ANNUAL_MEAN_TEMPERATURE = "wc2.1_5m_bio_1",
+  MEAN_DIURNAL_RANGE = "wc2.1_5m_bio_2",
+  ISOTHERMALITY = "wc2.1_5m_bio_3",
+  TEMPERATURE_SEASONALITY = "wc2.1_5m_bio_4",
+  MAX_TEMPERATURE_OF_WARMEST_MONTH = "wc2.1_5m_bio_5",
+  MIN_TEMPERATURE_OF_COLDEST_MONTH = "wc2.1_5m_bio_6",
+  TEMPERATURE_ANNUAL_RANGE = "wc2.1_5m_bio_7",
+  MEAN_TEMPERATURE_OF_WETTEST_QUARTER = "wc2.1_5m_bio_8",
+  MEAN_TEMPERATURE_OF_DRIER_QUARTER = "wc2.1_5m_bio_9",
+  MEAN_TEMPERATURE_OF_WARMEST_QUARTER = "wc2.1_5m_bio_10",
+  MEAN_TEMPERATURE_OF_COLDEST_QUARTER = "wc2.1_5m_bio_11",
+  ANNUAL_PRECIPITATION = "wc2.1_5m_bio_12",
+  PRECIPITATION_OF_WETTEST_MONTH = "wc2.1_5m_bio_13",
+  PRECIPITATION_OF_DRIEST_MONTH = "wc2.1_5m_bio_14",
+  PRECIPITATION_SEASONALITY = "wc2.1_5m_bio_15",
+  PRECIPITATION_OF_WETTEST_QUARTER = "wc2.1_5m_bio_16",
+  PRECIPITATION_OF_DRIEST_QUARTER = "wc2.1_5m_bio_17",
+  PRECIPITATION_OF_WARMEST_QUARTER = "wc2.1_5m_bio_18",
+  PRECIPITATION_OF_COLDEST_QUARTER = "wc2.1_5m_bio_19"
+)
---
+++ scripts/main.R
@@ -0,0 +1,83 @@
+source("scripts/bioclimatic_variable.R")
+source("scripts/map_generator.R")
+
+# Define study area polygon where vectors are geographical coordinates
+study_area <- st_sfc(
+  st_polygon(list(matrix(c(
+    -11.91717, 34.35806,
+    46.05072, 34.35806,
+    46.05072, 71.73288,
+    -11.91717, 71.73288,
+    -11.91717, 34.35806
+  ), ncol = 2, byrow = TRUE))),
+  crs = 4326
+)
+
+draw_laphria_gibbosa_distribution <- function(study_area) {
+  # Load species occurrence data and environmental layers
+  species_data <- read.csv("distribution/Laphria gibbosa.csv", sep = "\t", header = TRUE)
+  
+  # Bioclimatic variables used in the analysis according to Laphria gibbosa ecology
+  bioclim_vars <- c(
+    BioClimaticVariable$ANNUAL_MEAN_TEMPERATURE,
+    BioClimaticVariable$MAX_TEMPERATURE_OF_WARMEST_MONTH,
+    BioClimaticVariable$MIN_TEMPERATURE_OF_COLDEST_MONTH,
+    BioClimaticVariable$TEMPERATURE_ANNUAL_RANGE,
+    BioClimaticVariable$MEAN_TEMPERATURE_OF_WARMEST_QUARTER,
+    BioClimaticVariable$MEAN_TEMPERATURE_OF_COLDEST_QUARTER,
+    BioClimaticVariable$ANNUAL_PRECIPITATION,
+    BioClimaticVariable$PRECIPITATION_OF_WETTEST_MONTH,
+    BioClimaticVariable$PRECIPITATION_OF_DRIEST_MONTH,
+    BioClimaticVariable$PRECIPITATION_SEASONALITY
+  )
+  
+  # Load predicted climatic data for 2040-2060. EC-Earth3-Veg good for Europe.
+  future_bioclim_stack <- rast("climate/wc2.1_5m_bioc_EC-Earth3-Veg_ssp245_2041-2060.tif")
+  
+  generate_distribution_maps(
+    study_area,
+    species_data,
+    bioclim_vars,
+    future_bioclim_stack,
+    draw_plot_sequentially = TRUE,
+    currentDistributionTitle = "Current distribution of Laphria gibbosa",
+    futureDistributionTitle = "Predicted distribution of Laphria gibbosa 2040-2060, SSP245"
+  )
+}
+
+draw_sarcosoma_globossum_distribution <- function(study_area) {
+  # Load species occurrence data and environmental layers
+  species_data <- read.csv("distribution/Sarcosoma globossum.csv", sep = "\t", header = TRUE)
+  
+  # Bioclimatic variables used in the analysis according to Sarcosoma globossum ecology
+  bioclim_vars <- c(
+    BioClimaticVariable$ANNUAL_MEAN_TEMPERATURE,
+    BioClimaticVariable$TEMPERATURE_SEASONALITY,
+    BioClimaticVariable$MIN_TEMPERATURE_OF_COLDEST_MONTH,
+    BioClimaticVariable$MEAN_TEMPERATURE_OF_WETTEST_QUARTER,
+    BioClimaticVariable$MEAN_TEMPERATURE_OF_WARMEST_QUARTER,
+    BioClimaticVariable$MEAN_TEMPERATURE_OF_COLDEST_QUARTER,
+    BioClimaticVariable$ANNUAL_PRECIPITATION,
+    BioClimaticVariable$PRECIPITATION_SEASONALITY,
+    BioClimaticVariable$PRECIPITATION_OF_WETTEST_QUARTER,
+    BioClimaticVariable$PRECIPITATION_OF_DRIEST_QUARTER
+  )
+  
+  # Load predicted climatic data for 2040-2060. UKESM1-0-LL good for Europe and focuses on precipitation+temperature
+  future_bioclim_stack <- rast("climate/wc2.1_5m_bioc_UKESM1-0-LL_ssp245_2041-2060.tif")
+  
+  generate_distribution_maps(
+    study_area,
+    species_data,
+    bioclim_vars,
+    future_bioclim_stack,
+    draw_plot_sequentially = TRUE,
+    currentDistributionTitle = "Current distribution of Sarcosoma globossum",
+    futureDistributionTitle = "Predicted distribution of Sarcosoma globossum 2040-2060, SSP245"
+  )
+}
+
+# Call drawing maps
+
+draw_laphria_gibbosa_distribution(study_area)
+draw_sarcosoma_globossum_distribution(study_area)
